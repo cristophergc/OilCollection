9a24465febbe81e07adadf545a8a8f66f4c73bd9
Cristopher-Germuts <cristophergc@hotmail.com>
2023-08-10 08:29:43 +1200

    feature/add menu dashboard

--- app/src/main/java/com/example/oilcollection/features/DashboardActivity.kt
+++ app/src/main/java/com/example/oilcollection/features/DashboardActivity.kt
@@ -2,23 +2,39 @@ package com.example.oilcollection.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.oilcollection.R
 import com.example.oilcollection.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
 
-    private var binding: ActivityDashboardBinding? = null
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
-        setSupportActionBar(binding?.toolbarDashboard)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        binding?.toolbarDashboard?.setNavigationOnClickListener { handleBackPressed() }
+        setContentView(R.layout.activity_dashboard)
 
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.drawer_layout)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    private fun handleBackPressed(){
-        finish()
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+                true
+            } else super.onOptionsItemSelected(item)
+        }
     }
-}
